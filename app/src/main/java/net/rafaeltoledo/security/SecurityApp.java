67d2cce3d6cab6404e7dbc659937aca721eb9116
Rafael Toledo <rafaeldtoledo@gmail.com>
2017-05-03 12:19:48 -0300

    Adds a simple use of Hawk

--- app/src/main/java/net/rafaeltoledo/security/SecurityApp.java
+++ app/src/main/java/net/rafaeltoledo/security/SecurityApp.java
@@ -1,6 +1,7 @@
 package net.rafaeltoledo.security;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.orhanobut.hawk.Hawk;
 
@@ -8,10 +9,17 @@ import net.sqlcipher.database.SQLiteDatabase;
 
 public class SecurityApp extends Application {
 
+    private static final String TAG = SecurityApp.class.getSimpleName();
+    private static final String PREF_TIMES = "net.rafaeltoledo.security.PREF_TIMES";
+
     @Override
     public void onCreate() {
         super.onCreate();
         Hawk.init(this).build();
         SQLiteDatabase.loadLibs(this);
+
+        int times = Hawk.get(PREF_TIMES, 0) + 1;
+        Log.d(TAG, "Psss! Let me tell a secret: you opened this app " + times + " times.");
+        Hawk.put(PREF_TIMES, times);
     }
 }
